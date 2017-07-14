816b5975da3ae879d36549a49c82c6fb24a5f9d6
Dinuka Jayasuriya <dinukapps@gmail.com>
2017-07-14 14:58:14 +0800

    Initial Commit

---
+++ app/src/main/java/com/earthling/atminput/ATMEditText.java
@@ -0,0 +1,90 @@
+package com.earthling.atminput;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+
+import java.text.NumberFormat;
+
+/**
+ * Created by dinuka on 14/07/2017.
+ */
+
+public class ATMEditText extends AppCompatEditText {
+
+    private String current = "";
+    private ATMEditText editText = ATMEditText.this;
+    public String Currency = "";
+    public Boolean Spacing = false;
+    public Boolean Delimiter = false;
+
+    public ATMEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    public ATMEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ATMEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void init() {
+
+        this.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                if (!s.toString().equals(current)) {
+                    editText.removeTextChangedListener(this);
+
+                    String cleanString = s.toString().replaceAll("[$,.]", "").replaceAll(Currency, "");
+
+                    double parsed = Double.parseDouble(cleanString);
+
+                    String currencyFormat = "";
+                    if(Spacing){
+                        if(Delimiter){
+                            currencyFormat = Currency + ". ";
+                        }
+                        else {
+                            currencyFormat = Currency + " ";
+                        }
+                    }
+                    else{
+                        if(Delimiter){
+                            currencyFormat = Currency + ".";
+                        }
+                        else {
+                            currencyFormat = Currency;
+                        }
+                    }
+
+                    String formatted = NumberFormat.getCurrencyInstance().format((parsed / 100)).replace("$", currencyFormat);
+
+                    current = formatted;
+                    editText.setText(formatted);
+                    editText.setSelection(formatted.length());
+
+                    editText.addTextChangedListener(this);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/earthling/atminput/Currency.java
@@ -0,0 +1,16 @@
+package com.earthling.atminput;
+
+/**
+ * Created by dinuka on 14/07/2017.
+ */
+
+public class Currency {
+    public static String MALAYSIA = "RM";
+    public static String INDONESIA = "Rp";
+    public static String SRILANKA = "Rs";
+    public static String USA = "$";
+    public static String UK = "£";
+    public static String INDIA = "Rs";
+    public static String PHILIPPINES = "₱";
+    public static String PAKISTAN = "Rp";
+}
